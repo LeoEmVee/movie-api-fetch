4a7a2e1e680af693b9766bdc9bfc55099e61e01f
LeoEmVee <santito79@hotmail.com>
2022-04-02 00:02:48 +0200

    Add Layout component. Move Header to it. Update app.tsx accordingly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import Header from '../components/Header';
 import MovieWrapper from '../context/movies/moviesContext';
+import Layout from '../components/Layout';
 
 function MovieAPIFetchApp({ Component, pageProps }: any) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
     <div className="main__content">
       <MovieWrapper>
-        <Header />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </MovieWrapper>
     </div>
   );
